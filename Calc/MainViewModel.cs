4ffd4b8816f4d7d0d190138a7a11ccd31bc2bb03
Maletervi47 <vigowi@yandex.ru>
2023-12-07 22:23:46 +0500

    Изменения

---
+++ Calc/MainViewModel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Calc
+{
+    public struct Result_operarion
+    {
+        public string Error;
+        public string Result;
+    }
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+
+            AddExampleCommand = new RelayCommand<string>((x) => AddElementToExample(x));
+            DeleteOneExampleCommand = new RelayCommand(DeleteOneExample, () => string.IsNullOrWhiteSpace(Example) == false);
+            DeleteExampleCommand = new RelayCommand(DeleteExample, () => string.IsNullOrWhiteSpace(Example) == false);
+            SolutionExampleCommand = new RelayCommand(SolutionExample, () => string.IsNullOrWhiteSpace(Example) == false);
+            ClearHistoryCommand = new RelayCommand(ClearHistory, () => string.IsNullOrWhiteSpace(History) == false);
+        }
+
+        private string _example;
+        private string _history;
+        private string _error;
+        public virtual RelayCommand<string> AddExampleCommand { get; }
+        public virtual RelayCommand DeleteOneExampleCommand { get; }
+        public virtual RelayCommand DeleteExampleCommand { get; }
+        public virtual RelayCommand SolutionExampleCommand { get; }
+        public virtual RelayCommand ClearHistoryCommand { get; }
+
+        private readonly Calcul _primer = new Calcul();
+
+
+        Result_operarion ans = new Result_operarion();
+
+        public string Example
+        {
+            get => _example;
+            set
+            {
+                if (_example == value) return;
+                _example = value;
+
+                OnPropertyChanged(Example);
+            }
+        }
+
+        public string History
+        {
+            get => _history;
+            set
+            {
+                if (_history == value) return;
+                _history = value;
+                OnPropertyChanged(History);
+            }
+        }
+
+        interface IExample
+        {
+            string History_ { get; }
+        }
+
+        public string Error
+        {
+            get => _error;
+            set
+            {
+                if (_error == value) return;
+                _error = value;
+                OnPropertyChanged(Error);
+            }
+        }
+
+        private void AddElementToExample(string element)
+        {
+            Error = null;
+            if (!string.IsNullOrEmpty(Example))
+            {
+                if (element == "+" || element == "-" || element == "*" || element == "/")
+                {
+                    char lastChar = Example[Example.Length - 1];
+                    if (lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/')
+                    {
+                        Example = Example.Remove(Example.Length - 1);
+                    }
+                }
+            }
+                Example += element;
+        }
+
+        private void DeleteOneExample()
+        {
+            if (Example.Length > 0)
+            {
+                Error = null;
+                Example = Example.Remove(1);
+            }
+        }
+
+        private void DeleteExample()
+        {
+            Error = null;
+            Example = null;
+        }
+
+        private void SolutionExample()
+        {
+            ans.Result = _primer.CalculWork(Example, ref ans).ToString();
+            Example = null;
+            if (ans.Error == null)
+            {
+                History += Example + "=" + ans.Result + "\n";
+                Example = ans.Result;
+            }
+            else
+            {
+                Error = ans.Error;
+            }
+        }
+
+        private void ClearHistory()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Calc/Parser.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calc
+{
+    public class Calcul
+    {
+        int Index_Symbol = 0;
+        private string String_Example = "";
+        int Current_Symbol;
+        char Symbol_Buffer;
+
+        private void Get_Symbol()
+        {
+            if (Index_Symbol + 1 > String_Example.Length)
+            {
+                Current_Symbol = '\0';
+            }
+            else
+            {
+                Current_Symbol = String_Example[Index_Symbol];
+                Index_Symbol++;
+            }
+        }
+        public double CalculWork(string Primer, ref Result_operarion ans)
+        {
+            String_Example = Primer;
+            Index_Symbol = 0;
+            Get_Symbol();
+            if (Current_Symbol == '\0')
+            {
+                ans.Error = "Empty Example";
+                return 0;
+            }
+            else
+            {
+                return MethodA(ref ans);
+            }
+        }
+
+        private double MethodA(ref Result_operarion ans) // + && -
+        {
+            double x = MethodB(ref ans);
+            while (Current_Symbol == '+' || Current_Symbol == '-')
+            {
+                Symbol_Buffer = (char)Current_Symbol;
+                Get_Symbol();
+                if (Symbol_Buffer == '+')
+                {
+                    x += MethodB(ref ans);
+                }
+                else
+                {
+                    x -= MethodB(ref ans);
+                }
+            }
+            return x;
+        }
+
+        private double MethodB(ref Result_operarion ans) // * && /
+        {
+            double x = MethodC(ref ans);
+            while (Current_Symbol == '*' || Current_Symbol == '/')
+            {
+                Symbol_Buffer = (char)Current_Symbol;
+                Get_Symbol();
+                if (Symbol_Buffer == '*')
+                {
+                    x *= MethodC(ref ans);
+                }
+                else
+                {
+                    x /= MethodC(ref ans);
+                }
+            }
+            return x;
+        }
+
+        private double MethodC(ref Result_operarion ans)  // ()
+        {
+            double x;
+            if (Current_Symbol == '(')
+            {
+                Get_Symbol();
+                x = MethodA(ref ans);
+                if (Current_Symbol == ')')
+                {
+                    Get_Symbol();
+                }
+                else
+                {
+                    ans.Error = "Error! Missing brecket";
+                    return 0;
+                }
+            }
+            else if (Current_Symbol == '-')
+            {
+                Get_Symbol();
+                x = -MethodC(ref ans);
+            }
+            else if (Current_Symbol >= '0' && Current_Symbol <= '9')
+            {
+                x = MethodD(ref ans);
+            }
+            else
+            {
+                ans.Error = "Error in recording the example";
+                return 0;
+            }
+            return x;
+        }
+
+        private double MethodD(ref Result_operarion ans)
+        {
+            string x = "";
+            while (Current_Symbol >= '0' && Current_Symbol <= '9')
+            {
+                x += (char)Current_Symbol;
+                Get_Symbol();
+                if (Current_Symbol == ',')
+                {
+                    x += (char)Current_Symbol;
+                    Get_Symbol();
+                }
+                if (Current_Symbol == '.')
+                {
+                    ans.Error = "Incorrect format";
+                    return 0;
+                }
+            }
+            return double.Parse(x);
+        }
+    }
+}
